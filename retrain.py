4967099f8099ba013908bbcba5743c9505d16d66
Dean Tare <deantare@hotmail.com>
2018-07-30 19:31:31 +1000

    LOL

--- retrain.py
+++ retrain.py
@@ -938,7 +938,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--image_dir',
       type=str,
-      default='dataset/jpeg',
+      default='dataset/jpg',
       help='Path to folders of labeled images.'
   )
   parser.add_argument(
